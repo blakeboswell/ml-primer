4ee7c6655fa71d5b6a1cdef1e829b4ed70fc0129
Blake Boswell <j.blake.boswell@gmail.com>
2015-11-13 19:10:58 -0500

    update to logistic script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Contains projects related to [Andrew Ng's Machine learning course](https://www.coursera.org/learn/machine-learning/home/welcome).
+### Contains projects related to [Andrew Ng's Machine learning course](https://www.coursera.org/learn/machine-learning/home/welcome).
 
 [Binomial Logistic Regression](http://blakeboswell.github.io/2015/11/10/Logistic-Regression.html)
 
--- binomial_logistic.R
+++ binomial_logistic.R
@@ -39,34 +39,40 @@ gradient.descent <- function(gradient){
   return(fit)
 }
 
+## initialize a gradient descent closure with the logistic gradient
+mdl <- gradient.descent(logistic.gradient)
+
 library(dplyr)
 x <- iris %>% mutate(Intercept = 1) %>% 
   select(Intercept, Sepal.Length, Sepal.Width) %>% as.matrix
 y <- iris %>% mutate(Setosa = as.integer(Species == 'setosa')) %>%
   select(Setosa) %>% as.matrix
 
-## use gradient descent to fit parameter
-mdl <- gradient.descent(logistic.gradient)
-theta_star <- mdl(x, y)$theta
+## fit theta to the training set
+theta_star <- mdl(x,y)$theta
 
 ## generate predictions using the best fit theta
-py <- g(as.matrix(x) %*% theta_star)
+py <- g(x %*% theta_star)
 
 ## apply decision rule to assign to 0,1 label
 yhat <- ifelse(py < 0.5, 0, 1)
 
 ## test to see how well the data fits
-sum(yhat == y) / nrow(y)
+sum(yhat == y) / length(y)
 
 ## check out the coefficients
 theta_star
 
+## generate data for plotly
 toJSON(x[yhat == 1, 2])
 toJSON(x[yhat != 1, 2])
 toJSON(x[yhat == 1, 3])
 toJSON(x[yhat != 1, 3])
 
 ## calculate linear coefficients
-slope = -theta_star[1] / theta_star[3]
-intercept = -theta_star[2] / theta_star[3]
+slope <- -theta_star[2] / theta_star[3]
+intercept <- -theta_star[1] / theta_star[3]
+
+
+
 
